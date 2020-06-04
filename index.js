80d1bff983e7e12a40ab4e4b6f790a1772aa1b38
mvertopoulos <mvertopoulos@gmail.com>
2020-06-04 15:25:00 +0300

    Minor fixes: * Converted all "var" to "const" or "let" * Translated some of comments from spanish * Updated package.json information

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-var GridFSService = require('./lib/gridfs-service');
+const GridFSService = require('./lib/gridfs-service');
 
 /**
  * Initialize storage component.
  */
 exports.initialize = function(dataSource, callback) {
-  var settings = dataSource.settings || {};
+  const settings = dataSource.settings || {};
 
-  var connector = new GridFSService(settings);
+  const connector = new GridFSService(settings);
   dataSource.connector = connector;
   dataSource.connector.dataSource = dataSource;
 
   connector.DataAccessObject = function () {};
 
-  for (var m in GridFSService.prototype) {
-    var method = GridFSService.prototype[m];
+  for (const m in GridFSService.prototype) {
+    const method = GridFSService.prototype[m];
     if (typeof method === 'function') {
       connector.DataAccessObject[m] = method.bind(connector);
-      for (var k in method) {
+      for (const k in method) {
         connector.DataAccessObject[m][k] = method[k];
       }
     }
